c80cc6b72f79bb727cad02578d8a3cd627f4b209
nitin <nitin.lakshman@live.com>
2023-09-20 08:57:34 -0400

    clean up.

--- Business/Repository.cs
+++ Business/Repository.cs
@@ -50,7 +50,6 @@ namespace Business
                 }
             }        
 
-
              return "Not implemented";
 
         }
--- Scenario/Program.cs
+++ Scenario/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Business;
 
+//User requests a tool set from agent
+// Tool set is passed to the Agent
+// Agent has a library of tools
+// If tool requested is matched string values to a potential method
+//  Tool implementation Enhancement to add generic support
+
+// Implementation
+// Add a class library project. Will use DIC add interfaces and define methods
 
 
 Repository repository = new Repository();
@@ -16,18 +24,9 @@ while (true)
 
     Console.WriteLine(input);
 
-    //value = Console.ReadLine();
-
     
 }
 
 
-//User requests a tool set from agent
-// Tool set is passed to the Agent
-// Agent has a library of tools
-// If tool requested is matched string values to a potential method
-//  Tool implementation Enhancement to add generic support
 
-// Implementation
-// Add a class library project. Will use DIC add interfaces and define methods
 
